f0bc1b398b845b9324eb97d7baf92fbed9e85b34
xclusive <ya.5164646777752015@yandex.ru>
2017-04-08 08:50:07 +0300

    Add bootstrap 3

--- src/WebShopBundle/Controller/DefaultController.php
+++ src/WebShopBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('WebShopBundle:Default:index.html.twig');
+        return $this->render('WebShopBundle:default:index.html.twig');
     }
 }
