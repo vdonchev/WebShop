a9f4891b2977024e569cff78d9afe7ff9794c707
xclusive <ya.5164646777752015@yandex.ru>
2017-04-09 20:13:39 +0300

    Add sidebar menu

---
+++ src/WebShopBundle/Controller/WebShopController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WebShopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use WebShopBundle\Entity\ProductCategory;
+
+class WebShopController extends Controller
+{
+    public function sidebarMenuAction()
+    {
+        $categories = $this->getDoctrine()->getRepository(ProductCategory::class)->findAll();
+        return $this->render("@WebShop/_categories_menu.html.twig", [
+            "categories" => $categories
+        ]);
+    }
+}
--- src/WebShopBundle/DataFixtures/ORM/WebShopFixtures.php
+++ src/WebShopBundle/DataFixtures/ORM/WebShopFixtures.php
@@ -18,4 +18,26 @@ class WebShopFixtures implements FixtureInterface
             ]
         );
     }
+
+    public function product()
+    {
+        $products = [
+            "iPhone 7 64GB",
+            "Samsung Galaxy S8",
+            "Acer Aspire 15KM",
+            "Corsair K70 RGB",
+            "Samsung QLED KLJ75",
+            "Beats Headphones RED",
+            "Kingston SSD 125GB",
+            "Sony ILCE-6500Z",
+            "Dell Alienware Area 51",
+            "Gorenje SB800W",
+            "Mirrorless Sony Alpha",
+            "GIGABYTE GeForce® GTX 1070",
+            "WD Purple 1TB",
+            "Монитор Philips 18.5''"
+        ];
+
+        return $products[array_rand($products)];
+    }
 }
